404c392f61a93962c5112af8b7f140e57d889bbf
Thomas Viana <thomasviana16@gmail.com>
2022-07-13 12:11:52 -0500

    Update domain barrel file.

--- lib/core/auth/domain.dart
+++ lib/core/auth/domain.dart
@@ -4,5 +4,3 @@ export './src/domain/account_type.dart';
 export './src/domain/auth_field.dart';
 export './src/domain/auth_repository.dart';
 export './src/domain/auth_response.dart';
-export './src/domain/credentials.dart';
-export './src/domain/required_parameters.dart';
