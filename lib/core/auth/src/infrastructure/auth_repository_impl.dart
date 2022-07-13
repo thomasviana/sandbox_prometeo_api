00035cf0f133997aefbbcb08162e254309e7507e
Thomas Viana <thomasviana16@gmail.com>
2022-07-13 12:13:53 -0500

    Update implementation with authfields (infrastructure).

--- lib/core/auth/src/infrastructure/auth_repository_impl.dart
+++ lib/core/auth/src/infrastructure/auth_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:dartz/dartz.dart';
 import 'package:http/http.dart' as http;
 import 'package:injectable/injectable.dart';
 import 'package:sandbox_prometeo/keys/keys.dart';
@@ -11,46 +12,58 @@ class AuthRepositoryImpl implements AuthRepository {
   static const String prometeoUrl = 'https://banking.sandbox.prometeoapi.com';
   static const String prometeoApiKey = PROMETEO_API_KEY;
 
+  String _provider = 'test';
+  List<AuthField> _authFields = [];
   @override
-  Future<RequiredParameters> getRequiredParameters(String bank) async {
+  Future<Option<List<AuthField>>> getAuthFields(String bank) async {
     try {
       final response = await http.get(
-        Uri.parse(prometeoUrl + '/provider/test/'),
+        Uri.parse(prometeoUrl + '/provider/$bank/'),
         headers: <String, String>{
           'Accept': 'application/json',
           'Content-Type': 'application/x-www-form-urlencoded',
           'X-API-Key': prometeoApiKey,
         },
       );
-      final parsed = jsonDecode(response.body);
-      final requiredParameters = RequiredParameters.fromJson(parsed);
-      return Future.value(requiredParameters);
+      final status = jsonDecode(response.body)['status'] as String;
+      if (status == 'error') {
+        return const None();
+      } else {
+        final parsed =
+            jsonDecode(response.body)['provider']['auth_fields'] as List;
+        final authFields = parsed.map((e) => AuthField.fromMap(e)).toList();
+        _provider = bank;
+        _authFields = authFields;
+        return some(authFields);
+      }
     } catch (error) {
       throw Exception('Error getting posts from server: $error');
     }
   }
 
   @override
-  Stream<AuthResponse> checkAuthStatus(Credentials credentials) async* {
+  Stream<AuthResponse> checkAuthStatus(List<String> credentials) async* {
     try {
-      final response = await http.post(
-        Uri.parse(prometeoUrl + '/login/'),
-        headers: <String, String>{
-          'Accept': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'X-API-Key': prometeoApiKey,
-        },
-        body: {
-          'provider': credentials.provider,
-          'username': credentials.username,
-          'password': credentials.password,
-        },
-      );
+      final response = await http.post(Uri.parse(prometeoUrl + '/login/'),
+          headers: <String, String>{
+            'Accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-API-Key': prometeoApiKey,
+          },
+          body: buildBodyFromFields(credentials));
       final authResponse = AuthResponse.fromJson(response.body);
       yield authResponse;
     } catch (error) {
       throw Exception('Error getting posts from server: $error');
     }
+  }
+
+  Map<String, String> buildBodyFromFields(List<String> credentials) {
+    Map<String, String> body = {'provider': _provider};
+    for (var i = 0; i < _authFields.length; i++) {
+      body.putIfAbsent(_authFields[i].name, () => credentials[i]);
+    }
+    return body;
   }
 
   @override
